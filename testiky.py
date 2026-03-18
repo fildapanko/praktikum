6700e221c6a54fe56886ddbd706c48c872c4accd
fildapanko <filip.machacek@gmail.com>
2026-03-18 12:03:47 +0100

    testiky zmena

--- testiky.py
+++ testiky.py
@@ -1,1 +1,9 @@
+import pandas as pd
+# nacteni google tabulek
+sheet_id = "1GyY4H-OTBL_kVIBzqRwe_UpJX8blSdwkcHyNo-NSDso"
+gid = "0"
 
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+
+df = pd.read_csv(url)
+print(df)
