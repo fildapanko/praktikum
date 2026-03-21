68cf3c7cc42adbd4d04cfa68fafc9e03d5e0e39f
fildapanko <filip.machacek@gmail.com>
2026-03-21 13:11:02 +0100

    vypocet kappy hotovo

--- praktikum_07.py
+++ praktikum_07.py
@@ -8,14 +8,6 @@ from uncertainties import unumpy as unp
 from scipy import stats
 
 
-# nacteni google tabulek
-sheet_id = "1GyY4H-OTBL_kVIBzqRwe_UpJX8blSdwkcHyNo-NSDso"
-gid = "0"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
-
-df_tlaky = pd.read_csv(url)
-
-
 # funkce na Studentuv koeficient
 def StudCoef(confidence, dof): 
     """
@@ -29,24 +21,6 @@ def StudCoef(confidence, dof):
     """
     alpha = 1 - confidence
     return stats.t.ppf(1 - alpha/2, dof) 
-
-
-# nejistota typu A 
-def unc_A(data):
-    """
-    Výpočet nejistoty typu A
-    data : list nebo numpy array
-        opakovaná měření
-    return : float
-        nejistota typu A
-    """
-    data = np.array(data)
-    n = len(data)
-
-    s = np.std(data, ddof=1) 
-    u_A = s / np.sqrt(n)
-    
-    return u_A
 
 
 # nejistoty typu B pro digitalni pristroje
@@ -83,7 +57,18 @@ def unc_B_cteni(a):
     return u_B
 
 
+
 # clement desormes metoda
+
+
+# nacteni google tabulek
+sheet_id = "1GyY4H-OTBL_kVIBzqRwe_UpJX8blSdwkcHyNo-NSDso"
+gid = "0"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+
+df_tlaky = pd.read_csv(url)
+
+
 # tlakomer
 p1 = np.array(df_tlaky['p1'])
 p2 = np.array(df_tlaky['p2'])
@@ -97,29 +82,24 @@ p2_u = unp.uarray(p2, u_p2)
 
 kappa = p1_u/(p1_u - p2_u)
 
-values = unp.nominal_values(kappa)
-errors = unp.std_devs(kappa)
 
-weights = 1 / errors**2
-mean = np.sum(weights * values) / np.sum(weights)
-unc = np.sqrt(1 / np.sum(weights))
-unc = unc * StudCoef(0.9973, 9)
-kappa_u = uf(mean, unc)
+# funkce na vazeny prumer
+def weight_average(hodnota): # krajni
+    values = unp.nominal_values(hodnota)
+    errors = unp.std_devs(hodnota)
+    weights = 1 / errors**2
+    mean = np.sum(weights * values) / np.sum(weights)
+    unc = np.sqrt(1 / np.sum(weights))
+    unc = unc * StudCoef(0.9973, 9)
+    return uf(mean, unc)
 
-#print(f'Kappa z rozvoje a tlaku je: {kappa_u:.1u}')
+kappa_u = weight_average(kappa)
+print(f'Kappa z rozvoje a tlaku je: {kappa_u:.1u}')
 
 kappa = (unp.log((p1_u+p0_u)/p0_u)) / (unp.log((p1_u+p0_u)/(p2_u+p0_u)))
+kappa_u = weight_average(kappa)
+print(f'Kappa z plneho vypoctu je: {kappa_u:.1u}')
 
-values = unp.nominal_values(kappa)
-errors = unp.std_devs(kappa)
-
-weights = 1 / errors**2
-mean = np.sum(weights * values) / np.sum(weights)
-unc = np.sqrt(1 / np.sum(weights))
-unc = unc * StudCoef(0.9973, 9)
-kappa_u = uf(mean, unc)
-
-#print(f'Kappa z plneho vypoctu je: {kappa_u:.1u}')
 
 # u trubice
 h1 = np.array(df_tlaky['h1'])
@@ -132,17 +112,8 @@ h1_u = unp.uarray(h1, u_h1)
 h2_u = unp.uarray(h2, u_h2)
 
 kappa = h1_u/(h1_u - h2_u)
-
-values = unp.nominal_values(kappa)
-errors = unp.std_devs(kappa)
-
-weights = 1 / errors**2
-mean = np.sum(weights * values) / np.sum(weights)
-unc = np.sqrt(1 / np.sum(weights))
-unc = unc * StudCoef(0.9973, 9)
-kappa_u = uf(mean, unc)
-
-#print(f'Kappa z rozvoje a U trubice je: {kappa_u:.1u}')
+kappa_u = weight_average(kappa)
+print(f'Kappa z rozvoje a U trubice je: {kappa_u:.1u}')
 
 
 
@@ -161,36 +132,56 @@ df_zvuk = pd.read_csv(url)
 def linear_model(x, a, b):
     return a * x + b
 
+values = []
+errors = []
 
-# odhad parametru, hranice
-R_initial = abs(df_zvuk.loc[0,"f840"] - df_zvuk.loc[1,"f840"])
-b_initial = 0
-p0 = [R_initial, b_initial]
-bounds = (0, np.inf)
+# funkce na fitovani
+def fit_lin_plot(df, column, barva):
+    col = df[f'{column}'].dropna()
+    popt, pcov = curve_fit(linear_model, df.index[:len(col)].to_numpy(dtype=float)+1, col,p0=None, bounds=(-np.inf, np.inf))
+    A, b = popt # OPTimalni Parametry -- popt
+    A_u, b_u = np.sqrt(np.diag(pcov)) # nejistota parametru z kovariancni matice
 
+    values.append(abs(A*1e-3))
+    errors.append(abs(A_u*1e-3))
 
-# vypocet hodnot pro fit
-popt, pcov = curve_fit(linear_model, df_zvuk.index.to_numpy(dtype=float), df_zvuk["f840"], p0=p0, bounds=bounds)
-R, b = popt # OPTimalni Parametry -- popt
-R_u, b_u = np.sqrt(np.diag(pcov)) # nejistota parametru z kovariancni matice
-R = uf(R, R_u)
-b = uf(b, b_u)
+    A = uf(A, A_u)
+    b = uf(b, b_u)
+
+    U_fit = np.linspace(1, len(col), 100)
+    ax.plot(U_fit, linear_model(U_fit, *popt), label=fr"Lineární fit: $\lambda / 2$ = {A:.1uPL} $mm$", color=f"{barva}")
+
 
 
 # graf hodnot
 fig, ax = plt.subplots(figsize=(16, 9))
-ax.set_title("lambda pul", fontsize=25, pad=15)
+ax.set_title("Poloha maxim $D_i$", fontsize=25, pad=15)
 ax.set_xlabel(fr"$i$", fontsize=20)
-ax.set_ylabel(fr"$D$", fontsize=20)
-ax.scatter(df_zvuk.index, df_zvuk["f840"],marker="o",label="Naměřené hodnoty", color="blue", s=100)
+ax.set_ylabel(fr"$D (mm)$", fontsize=20)
+ax.scatter(df_zvuk.index+1, df_zvuk["f840"],marker="o",label="Frekvence 840 $Hz$", color="blue", s=100)
+ax.scatter(df_zvuk.index+1, df_zvuk["f995"],marker="o",label="Frekvence 995 $Hz$", color="red", s=100)
+ax.scatter(df_zvuk.index+1, df_zvuk["f1095"],marker="o",label="Frekvence 1095 $Hz$", color="green", s=100)
+ax.scatter(df_zvuk.index+1, df_zvuk["f1255"],marker="o",label="Frekvence 1255 $Hz$", color="purple", s=100)
 ax.tick_params(labelsize=15)
 ax.grid(True, alpha=0.7)
 
 
 # vykresleni fitu
-U_fit = np.linspace(df_zvuk["f840"].min(), df_zvuk["f840"].max(), 100)
-
-ax.plot(U_fit, linear_model(U_fit, *popt), label=fr"Lineární fit: $R$ = {R:.1uPL} $\Omega$", color="red")
+fit_lin_plot(df_zvuk, 'f840', '#0072B2')
+fit_lin_plot(df_zvuk, 'f995', "#D55E00")
+fit_lin_plot(df_zvuk, 'f1095', '#009E73')
+fit_lin_plot(df_zvuk, 'f1255', '#CC79A7')
 
 ax.legend(fontsize=15)
-plt.show()
+#plt.savefig(r"C:\Users\Admin\Downloads\lambdapul.png", dpi=300)
+#plt.show()
+
+
+# vypocet kappy z fitu
+lambda2 = unp.uarray(values , errors)
+freq = np.array([840, 995, 1095, 1255])
+rho = 1.167065
+
+kappa = (rho * 4 * (lambda2**2) * freq**2)/p0_u
+kappa_u = weight_average(kappa)
+print(f'Kappa z fitu je: {kappa_u:.1u}')
