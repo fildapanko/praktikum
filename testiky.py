7375f2daeffb32b8ecf516d3f2aeb6b8bbe5e7bd
fildapanko <filip.machacek@gmail.com>
2026-03-23 17:04:56 +0100

    smazano testiky

--- testiky.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-# nacteni google tabulek
-sheet_id = "1SabDjWsPIsUqenpKLWSS_sL0DrfNNjyDOQ5PH-PDqho"
-gid = "1924903675"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
-df_infra = pd.read_csv(url)
-
-print(df_infra.columns)
