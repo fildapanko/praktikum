5b2e6e035945c1c92b457b00e29100fd49953b1d
fildapanko <filip.machacek@gmail.com>
2026-04-23 21:08:23 +0200

    kyvadlo

--- praktikum_04.py
+++ praktikum_04.py
@@ -16,7 +16,6 @@ def polynom_model(x, a, b, c):
     return a*x**2 + b*x + c
 
 # funkce na fitovani
-
 def fit_poly(x, y):
     popt, pcov = curve_fit(polynom_model, x, y)
     err = np.sqrt(np.diag(pcov))
@@ -28,7 +27,7 @@ a_u, b_u, c_u = err1
 a1 = uf(a01, a_u)
 b1 = uf(b01, b_u)
 c1 = uf(c01, c_u)
-U_fit1 = np.linspace(min(df_kyvadlo['z1']), max(df_kyvadlo['z1']), 200)
+z_fit1 = np.linspace(min(df_kyvadlo['z1']), max(df_kyvadlo['z1']), 200)
 
 popt2, err2 = fit_poly(df_kyvadlo['z2'], df_kyvadlo['T2'])
 a02, b02, c02 = popt2
@@ -36,39 +35,58 @@ a_u, b_u, c_u = err2
 a2 = uf(a02, a_u)
 b2 = uf(b02, b_u)
 c2 = uf(c02, c_u)
-U_fit2 = np.linspace(min(df_kyvadlo['z2']), max(df_kyvadlo['z2']), 200)
+z_fit2 = np.linspace(min(df_kyvadlo['z2']), max(df_kyvadlo['z2']), 200)
 
+def intersection(p1, p2):
+    coeffs = np.array(p1) - np.array(p2)
+    roots = np.roots(coeffs)
+
+    res = []
+    for r in roots:
+        if np.isreal(r) and r.real > 0:
+            x = r.real
+            y = np.polyval(p1, x)
+            res.append((x, y))
+    return res
+
+intersections = intersection(popt1, popt2)
 
 # graf a fit
 fig, ax = plt.subplots(figsize=(16, 9))
-ax.set_xlabel(fr'$z\,(mm)$', fontsize=20)
-ax.set_ylabel(fr'$t\,(s)$', fontsize=20)
-ax.scatter(df_kyvadlo['z1'], df_kyvadlo['T1'],marker='.',label='Naměřené hodnoty', color='blue', s=100)
-ax.plot(U_fit1, polynom_model(U_fit1, *popt1), label=fr'Fit polynomem ($ax^2+bx+c$)', color='cyan')
+ax.scatter(df_kyvadlo['z1'], df_kyvadlo['T1'], marker='.', s=100, label='Naměřené hodnoty $T_1$', color='blue')
+ax.scatter(df_kyvadlo['z2'], df_kyvadlo['T2'], marker='.', s=100, label='Naměřené hodnoty $T_2$', color='red')
+ax.plot(z_fit1, polynom_model(z_fit1, *popt1), label='Fit polynomem ($ax^2+bx+c$)', color='cyan')
+ax.plot(z_fit2, polynom_model(z_fit2, *popt2), label='Fit polynomem ($ax^2+bx+c$)', color='orange')
 
-
-ax.scatter(df_kyvadlo['z2'], df_kyvadlo['T2'],marker='.',label='Naměřené hodnoty', color='red', s=100)
-ax.plot(U_fit2, polynom_model(U_fit2, *popt2), label=fr'Fit polynomem ($ax^2+bx+c$)', color='orange')
-
-# rozdilovy polynom
-coeffs = [a01 - a02, b01 - b02, c01 - c02]
-roots = np.roots(coeffs)
-intersections = [
-    (r.real, a01*r.real**2 + b01*r.real + c01)
-    for r in roots
-    if np.isreal(r) and r.real > 0
-]
-# vykresleni pruseciku
 for x, y in intersections:
-    plt.scatter(x, y, marker='x',label='Průsečík', color='green', s=200)
-    plt.annotate(
-    f'({x:.3f}, {y:.3f})',
-    (x, y),
-    xytext=(8, -15),
-    textcoords='offset points',
-)
+    ax.scatter(x, y, marker='x', color='green', s=200)
+    ax.annotate(
+        f"({x:.3f}, {y:.3f})",
+        (x, y),
+        xytext=(8, -15),
+        textcoords='offset points'
+    )
 
-ax.tick_params(labelsize=15)
+ax.set_xlabel(r'$z\,(mm)$', fontsize=20)
+ax.set_ylabel(r'$t\,(s)$', fontsize=20)
 ax.grid(True, alpha=0.7)
 ax.legend(fontsize=15)
+ax.tick_params(labelsize=15)
+
 plt.savefig(r'C:\Users\Admin\Downloads\kyvadlo.png', dpi=300, bbox_inches='tight')
+
+A = a1 - a2
+B = b1 - b2
+C = c1 - c2
+
+D = B**2 - 4*A*C
+
+x1 = (-B + D**0.5) / (2*A)
+x2 = (-B - D**0.5) / (2*A)
+
+T0 = a1*x1**2 + b1*x1 + c1
+
+delka = uf(98.9*1e-2, 0.0001)
+
+g = (4*np.pi**2 * delka)/(T0**2)
+print(f'Graviační zrychlení je: {g:.2uPL}')
